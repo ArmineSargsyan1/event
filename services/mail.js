dace8f28881324525d4dee5492a0c4a1a22e453e
ArmineSargsyan1 <armine9086@gmail.com>
2026-07-11 18:40:47 +0400

    change

--- services/mail.js
+++ services/mail.js
@@ -2,11 +2,20 @@ import path from 'path';
 import ejs from 'ejs';
 import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
-
+import dns from "node:dns/promises";
 dotenv.config();
 
 const { EMAIL, EMAIL_PASSWORD, EMAIL_HOST} = process.env;
 
+// const transporter = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     user: EMAIL,
+//     pass: EMAIL_PASSWORD,
+//   },
+// });
+
+console.log(await dns.lookup("smtp.gmail.com", { all: true }));
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
@@ -17,6 +26,17 @@ const transporter = nodemailer.createTransport({
     pass: EMAIL_PASSWORD,
   },
 });
+console.log({
+  EMAIL,
+  hasPassword: !!EMAIL_PASSWORD,
+});
+
+try {
+  await transporter.verify();
+  console.log("✅ SMTP VERIFIED");
+} catch (err) {
+  console.error("❌ SMTP VERIFY ERROR:", err);
+}
 
 export const sendMail = async ({to, subject, template, templateData = {}, attachments = []}) => {
 
